77d21f60ff0dd3f7314dd9be5c0929dca217f984
Pabl1k <poiumazaya@gmail.com>
2025-05-17 11:27:48 +0300

    SubmitButton

---
+++ src/shared/ui/SubmitButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button
+    class="w-full h-[50px] bg-submit-button-background rounded-sm border border-submit-button-background text-lg font-bold text-form-background uppercase cursor-pointer hover:opacity-80 transition"
+  >
+    <slot />
+  </button>
+</template>
